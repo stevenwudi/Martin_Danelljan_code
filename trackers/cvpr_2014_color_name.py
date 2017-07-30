454cb674221a7a0119f5e6c9328258fe65dcbc31
stevenwudi <stevenwudi@gmail.com>
2017-07-30 01:15:51 -0400

    Update training procedure, TODO: finish detection stage

--- trackers/cvpr_2014_color_name.py
+++ trackers/cvpr_2014_color_name.py
@@ -8,6 +8,7 @@ email: stevenwudi@gmail.com  2017/08/01
 """
 import numpy as np
 import h5py
+import matplotlib.pyplot as plt
 import os
 
 
@@ -63,6 +64,9 @@ class cvpr_2014_color_name:
         self.name = 'cvpr_2014_color_name'
 
     def train(self, im, init_rect):
+        """
+        train the appearance model from the frist frame
+        """
         self.pos = [init_rect[1] + init_rect[3] / 2., init_rect[0] + init_rect[2] / 2.]
         self.res.append(init_rect)
         self.target_sz = np.asarray(init_rect[2:])
@@ -71,17 +75,17 @@ class cvpr_2014_color_name:
         self.patch_size = np.floor(self.target_sz * (1 + self.padding))
         self.output_sigma = np.sqrt(np.prod(self.target_sz)) * self.output_sigma_factor
 
-        grid_y = np.arange(np.floor(self.target_sz[0]) - np.floor(self.target_sz[0] / 2))
-        grid_x = np.arange(np.floor(self.target_sz[1]) - np.floor(self.target_sz[0] / 2))
+        grid_y = np.arange(np.floor(self.patch_size[0])) - np.floor(self.patch_size[0] / 2)
+        grid_x = np.arange(np.floor(self.patch_size[1])) - np.floor(self.patch_size[1] / 2)
         rs, cs = np.meshgrid(grid_x, grid_y)
         self.y = np.exp(-0.5 / self.output_sigma ** 2 * (rs ** 2 + cs ** 2))
         self.yf = self.fft2(self.y)
         # store pre-computed cosine window
-        self.cos_window = np.outer(np.hanning(self.yf.shape[0]), np.hanning(self.yf.shape[1]))
+        self.cos_window = np.outer(np.hanning(self.patch_size[0]), np.hanning(self.patch_size[1]))
 
         # extract the feature map of the local image patch to train the classifer
         self.im_crop = self.get_subwindow(im, self.pos, self.patch_size)
-        self.get_features()
+        self.xo_npca, self.xo_pca = self.get_features(self.im_crop)
 
         # initiliase the appearance
         self.z_npca = self.xo_npca
@@ -104,17 +108,53 @@ class cvpr_2014_color_name:
             self.projection_matrix = U[:, :self.num_compressed_dim]
             self.projection_variances = S[:self.num_compressed_dim, :self.num_compressed_dim]
             # initialise the old covariance matrix using the computed projection matrix and variance
-            self.old_cov_matrix = np.dot( np.dot( self.projection_matrix, self.projection_variances),
-                                          self.projection_matrix.T)
+            self.old_cov_matrix = np.dot(np.dot(self.projection_matrix, self.projection_variances),
+                                         self.projection_matrix.T)
 
-            # TODO: project the features of the new appearance example using the new projection matrix
+        # project the features of the new appearance example using the new projection matrix
+        self.x = self.feature_projection(self.xo_npca, self.xo_pca,
+                                         self.projection_matrix, self.cos_window)
+        self.xf = self.fft2(self.x)
+        # compute the compressed learnt appearance
+        self.zp = self.x
+        self.zpf = self.xf
 
-            # TODO: calculate the new classifier coefficients
+        # calculate the new classifier coefficients
+        self.kf = self.fft2(self.dense_gauss_kernel(self.sigma, self.xf, self.x))
+        self.alpha_num = np.multiply(self.yf, self.kf)
+        self.alpha_den = np.multiply(self.kf, (self.kf + self.lambda_value))
+
+        self.res.append(init_rect)
+
+    def detect(self, im, frame):
+
+        # extract the feature map of the local image patch
+        self.im_crop = self.get_subwindow(im, self.pos, self.patch_size)
+        self.xo_npca, self.xo_pca = self.get_features(self.im_crop)
+
+        # do the dimensionality reduction and windowing
+        self.x = self.feature_projection(self.xo_npca, self.xo_pca,
+                                         self.projection_matrix, self.cos_window)
+        self.xf = self.fft2(self.x)
+
+        # calculate the response of the classifier
+        self.kf = self.fft2(self.dense_gauss_kernel(self.sigma, self.zpf, self.zp, self.xf, self.x))
+        self.response = np.real(np.fft.ifft2(np.divide(np.multiply(self.alpha_num, self.kf), self.alpha_den)))
+
+        # TODO: target location is at the maximum response
+
+        # TODO: extract the feature map of the local image path to train the classifier
+        
+        # TODO: update the appearance
+
+        # TODO: put the dimensionality reduction into a function call...*[]:
 
 
 
-    def detect(self):
-        pass
+        # compute the compressed learnt appearance
+        self.zp = self.feature_projection(self.z_npca, self.z_pca,
+                                         self.projection_matrix, self.cos_window)
+
 
     def fft2(self, x):
         """
@@ -154,21 +194,23 @@ class cvpr_2014_color_name:
 
         return im[np.ix_(ys, xs)]
 
-    def get_features(self):
-
+    def get_features(self, im_crop):
+        xo_npca, xo_pca = None, None
         def rgb2gray(rgb):
             return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
         if self.non_compressed_features == 'gray':
-            self.xo_npca = rgb2gray(self.im_crop)/255. - 0.5
+            xo_npca = rgb2gray(im_crop)/255. - 0.5
 
         if self.compressed_features == 'cn':
-            xo_pca_temp = self.im2c(self.im_crop)
+            xo_pca_temp = self.im2c(im_crop)
             # 'F' , means to flatten in column-major
-            self.xo_pca = np.reshape(xo_pca_temp,
+            xo_pca = np.reshape(xo_pca_temp,
                                      (np.prod([xo_pca_temp.shape[0], xo_pca_temp.shape[1]]),
                                       xo_pca_temp.shape[2]),
                                      order='F')
+
+        return xo_npca, xo_pca
 
     def im2c(self, im):
         """
@@ -192,3 +234,59 @@ class cvpr_2014_color_name:
 
         out = np.reshape(self.w2c[index_im.astype('int')], (im.shape[0], im.shape[1], self.w2c.shape[1]), 'F')
         return out
+
+    def feature_projection(self, xo_npca, xo_pca, projection_matrix, cos_window):
+        """
+        Calcaultes the compressed feature map by mapping the PCA features with the projection matrix
+        and concatinates this with the non-PCA features. The feature map is than multiplied with a cosine-window.
+        :return:
+        """
+        if not self.compressed_features:
+            z = xo_npca
+        else:
+            # project the PCA-features using the projection matrix and reshape to a window
+            x_proj_pca = np.dot(xo_pca, projection_matrix).reshape(
+                (cos_window.shape[0], cos_window.shape[1], projection_matrix.shape[1]))
+
+            # concatinate the feature windows
+            if not self.non_compressed_features:
+                z = x_proj_pca
+            else:
+                # gray scale concatenate with color names
+                if len(xo_npca.shape) != len(x_proj_pca.shape):
+                    z = np.concatenate((xo_npca[:, :, None], x_proj_pca), axis=2)
+
+        features = np.multiply(z, cos_window[:, :, None])
+        return features
+
+    def dense_gauss_kernel(self, sigma, xf, x, zf=None, z=None):
+        """
+        Gaussian Kernel with dense sampling.
+        Evaluates a gaussian kernel with bandwidth SIGMA for all displacements
+        between input images X and Y, which must both be MxN. They must also
+        be periodic (ie., pre-processed with a cosine window). The result is
+        an MxN map of responses.
+
+        If X and Y are the same, ommit the third parameter to re-use some
+        values, which is faster.
+        :param sigma: feature bandwidth sigma
+        :param x:
+        :param y: if y is None, then we calculate the auto-correlation
+        :return:
+        """
+        N = xf.shape[0]*xf.shape[1]
+        xx = np.dot(x.flatten().transpose(), x.flatten())  # squared norm of x
+
+        if zf is None:
+            # auto-correlation of x
+            zf = xf
+            zz = xx
+        else:
+            zz = np.dot(z.flatten().transpose(), z.flatten())  # squared norm of y
+
+        xyf = np.multiply(zf, np.conj(xf))
+        xy = np.real(np.fft.ifft2(np.sum(xyf, axis=2)))
+
+        k = np.exp(-1. / sigma**2 * np.maximum(0, (xx + zz - 2 * xy)) / N)
+
+        return k
