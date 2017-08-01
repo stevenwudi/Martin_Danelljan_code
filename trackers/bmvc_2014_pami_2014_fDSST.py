4566eed02f5272db6fa97f1d5857d70ba335aeae
Di Wu <stevenwudi@gmail.com>
2017-08-01 05:07:11 -0400

    scale initialisation

--- trackers/bmvc_2014_pami_2014_fDSST.py
+++ trackers/bmvc_2014_pami_2014_fDSST.py
@@ -23,6 +23,8 @@ http://stevenwudi.github.io/
 email: stevenwudi@gmail.com  2017/08/01
 """
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class bmvc_2014_pami_2014_fDSST:
     def __init__(self,
@@ -37,11 +39,12 @@ class bmvc_2014_pami_2014_fDSST:
                  interpolate_response=1,
                  resize_factor=1,
                  number_of_scales=17,
-                 number_of_interp_scales=33,
+                 number_of_interp_scales=33.,
                  scale_model_factor=1.0,
                  scale_step=1.02,
                  scale_model_max_area=512,
                  s_num_compressed_dim='MAX',
+                 featureRatio=4.0,
                  visualisation=1,
                  ):
         """
@@ -62,6 +65,7 @@ class bmvc_2014_pami_2014_fDSST:
         :param scale_step: scale increment factor (denoted 'a" in the paper)
         :param scale_model_max_area: the maximum size of scale examples
         :param s_num_compressed_dim: number of compressed scale feature dimensions
+        :param featureRatio: HOG window size
         :param visualisation: flag for visualistion
         """
         self.padding = padding
@@ -81,11 +85,80 @@ class bmvc_2014_pami_2014_fDSST:
         self.scale_model_max_area = scale_model_max_area
         self.s_num_compressed_dim = s_num_compressed_dim
         self.visualisation = visualisation
+        self.featureRatio = featureRatio
+
+        if self.number_of_scales > 0:
+            self.scale_sigma = self.number_of_interp_scales * self.scale_sigma_factor
+            self.scale_exp = (np.arange(self.number_of_scales) - np.floor(self.number_of_scales/2)) * \
+                             (self.number_of_interp_scales/self.number_of_scales)
+            self.interp_scale_exp = np.arange(self.number_of_interp_scales) - np.floor(self.number_of_interp_scales/2)
+
+            self.scaleSizeFactors = self.scale_step ** self.scale_exp
+            self.interpScaleFactors = self.scale_step ** self.interp_scale_exp
+
+            self.ys = np.exp(-0.5 * self.scale_exp**2 / self.scale_sigma**2)
+            self.ysf = np.fft.fft(self.ys)
+            self.scale_wnidow = np.hanning(self.ysf.shape[0])
+
+        self.name = 'bmvc_2014_pami_2014_fDSST'
 
     def train(self, im, init_rect):
-        # TODO: write training function
-        pass
+        self.pos = [init_rect[1] + init_rect[3] / 2., init_rect[0] + init_rect[2] / 2.]
+        self.res.append(init_rect)
+        self.target_sz = np.asarray(init_rect[2:])
+        self.target_sz = self.target_sz[::-1]
+        self.init_target_sz = self.target_sz
+        self.im_sz = im.shape[:2]
+
+        if np.prod(self.target_sz.shape) > self.translation_model_max_area:
+            self.currentScaleFactor = np.sqrt(np.prod(self.init_target_sz) / self.translation_model_max_area)
+        else:
+            self.currentScaleFactor = 1.0
+        # target size at the initial scale
+        self.base_target_sz = self.target_sz / self.currentScaleFactor
+        # window size, taking padding into account
+        self.patch_size = np.floor(self.base_target_sz * (1 + self.padding))
+        self.output_sigma = np.sqrt(np.prod(np.floor(self.base_target_sz / self.featureRatio))) * self.output_sigma_factor
+        self.use_sz = np.floor(self.patch_size/self.featureRatio)
+
+        grid_y = np.arange(np.floor(self.use_sz[0])) - np.floor(self.use_sz[0] / 2)
+        grid_x = np.arange(np.floor(self.use_sz[1])) - np.floor(self.use_sz[1] / 2)
+        rs, cs = np.meshgrid(grid_x, grid_y)
+        self.y = np.exp(-0.5 / self.output_sigma ** 2 * (rs ** 2 + cs ** 2))
+        self.yf = self.fft2(self.y)
+
+        if self.number_of_scales > 0:
+            # make sure the scale model is not too large so as to save computation time
+            if self.scale_model_factor**2 * np.floor(self.init_target_sz) > self.scale_model_max_area:
+                self.scale_model_factor = np.sqrt(self.scale_model_max_area/np.prod(self.init_target_sz))
+
+            # set the scale model size
+            self.scale_model_sz = np.floor(self.init_target_sz * self.scale_model_factor)
+            # force reasonable scale changes
+            self.min_scale_factor = self.scale_step ** np.ceil(np.log(np.max(5. / self.patch_size)) / np.log(self.scale_step))
+            self.max_scale_factor = self.scale_step ** np.floor(np.log(np.min(np.array([im.shape[0], im.shape[1]]) * 1.0 / self.base_target_sz)) / np.log(self.scale_step))
+
+            #if self.s_num_compressed_dim == 'MAX':
+
+        # TODO: Compute coefficients for the tranlsation filter
+
+        # TODO: Compute coefficents for the scale filter
+
+
+
+
+
+
+
 
     def detect(self, im, frame):
         # TODO: write detection function
         pass
+
+    def fft2(self, x):
+        """
+        FFT transform of the first 2 dimension
+        :param x: M*N*C the first two dimensions are used for Fast Fourier Transform
+        :return:  M*N*C the FFT2 of the first two dimension
+        """
+        return np.fft.fft2(x, axes=(0, 1))
